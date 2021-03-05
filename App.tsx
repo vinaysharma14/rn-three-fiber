2df391ace20261b171a1b1b117ba8df0910faf66
Vinay Sharma <vinay@geekyants.com>
2021-03-05 15:41:54 +0530

    cube example with react spring

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, FC } from 'react';
+
+import { a, useSpring } from '@react-spring/three';
 import { Canvas, useFrame } from 'react-three-fiber';
 
 interface Props {
   position: [number, number, number]
 }
 
-const Box = ({ position }: Props) => {
+const ReactSpringBox: FC = () => {
+  const [active, setActive] = useState(0)
+
+  // create a common spring that will be used later to interpolate other values
+  const { spring } = useSpring({
+    spring: active,
+    config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 }
+  })
+
+  // interpolate values from common springs
+  const scale = spring.to([0, 1], [1, 5])
+  const rotation = spring.to([0, 1], [0, Math.PI])
+  const color = spring.to([0, 1], ['#6246ea', '#e45858'])
+
+  return (
+    // using a from react-spring will animate our component
+    <a.group position-y={scale}>
+      <a.mesh rotation-y={rotation} scale-x={scale} scale-z={scale} onClick={() => setActive(Number(!active))}>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <a.meshStandardMaterial roughness={0.5} attach="material" color={color} />
+      </a.mesh>
+    </a.group>
+  )
+}
+
+const Box: FC<Props> = ({ position }) => {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
 
@@ -35,11 +62,19 @@ const Box = ({ position }: Props) => {
 
 export default () => {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+
+      <Canvas colorManagement camera={{ position: [-10, 10, 10], fov: 35 }}>
+        <ambientLight />
+        <pointLight position={[-10, 10, -10]} castShadow />
+        <ReactSpringBox />
+      </Canvas>
+    </>
   );
 }
